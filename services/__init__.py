bad75b9623972c1300493b4949bfb371ae54647f
DasunNethsara-04 <techsaralk.pro@gmail.com>
2026-03-02 11:07:37 +0530

    fix: __init__.py updated for new functions

--- services/__init__.py
+++ services/__init__.py
@@ -1,22 +1,27 @@
 from .auth import (
     build_github_redirect_url,
     handle_supabase_callback,
+    handle_github_callback,
+    log_auth_error,
     create_session_cookie,
-    decode_session_cookie
+    decode_session_cookie,
 )
-from .user import auto_register_user, verify_user, get_qr_image
+from .user import auto_register_user, verify_user, get_qr_image, generate_qr_data_url
 from .mail import send_qr_email
 from .event import get_active_event, get_event_by_id
 
 __all__ = [
-    "register_user",
+    "auto_register_user",
     "verify_user",
     "get_qr_image",
+    "generate_qr_data_url",
     "send_qr_email",
     "build_github_redirect_url",
     "handle_supabase_callback",
+    "handle_github_callback",
+    "log_auth_error",
     "create_session_cookie",
     "decode_session_cookie",
     "get_active_event",
-    "get_event_by_id"
+    "get_event_by_id",
 ]
